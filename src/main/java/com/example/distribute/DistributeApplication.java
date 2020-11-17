150931da149f3d19f5934a63de1f9e66fab3f474
jijungkeun <gjk0508@gmail.com>
2020-11-18 01:26:50 +0900

    init

---
+++ src/main/java/com/example/distribute/DistributeApplication.java
@@ -0,0 +1,15 @@
+package com.example.distribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributeApplication {
+
+    public static void main(String[] args) {
+        System.out.println("hello");
+        SpringApplication.run(DistributeApplication.class, args);
+
+    }
+
+}
